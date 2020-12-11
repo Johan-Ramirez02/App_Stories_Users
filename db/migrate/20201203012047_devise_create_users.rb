9c334ba61ed143bd32639764095fb9ed57b96af9
Johan Ramirez <johkcolom@gmail.com>
2020-12-11 09:49:09 -0300

    Corrigiendo sqlite3 por pg

--- db/migrate/20201203012047_devise_create_users.rb
+++ db/migrate/20201203012047_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       ## Database authenticatable
       t.string :name,               null: false, default: ""
       t.boolean :admin,              
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: true, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
